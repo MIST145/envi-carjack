4107206992412c40d9d8ae176d3105ff34082299
Mist <114344332+MIST145@users.noreply.github.com>
2023-03-01 01:41:50 +0000

    Add files via upload

---
+++ envi-carjack/config.lua
@@ -0,0 +1,12 @@
+Config = {}
+
+Config.Framework = 'esx' -- 'esx' or 'qb'
+
+Config.TakeVehWeapons = {
+    [`WEAPON_NIGHTSTICK`] = true, -- add/change weapons here
+
+}
+
+Config.Target = false --'ox_target' -- 'ox_target', 'qtarget', 'qb-target' or false --( /carjack command )
+
+Config.AlwaysUnlock = true -- always unlock vehicle? (add your vehicle keys triggers in client.lua)
